459e2a7497b978f170248bc95748291893601fe5
SNOWS <snows@ZRP-N012.localdomain>
2023-09-04 09:58:56 -0300

    download data generalized

--- downloadFile.js
+++ downloadFile.js
@@ -1,15 +1,31 @@
 const axios = require('axios');
 const fs = require('fs');
+const path = require('path');
 
-// Replace this URL with the link you want to download content from
+// Replace this URL with the link to the file you want to download
 const url = 'https://app.anm.gov.br/dadosabertos/SIGMINE/AREA_SERVIDAO.zip';
-
-// Define the filename where you want to save the downloaded content
-const filename = 'downloaded-content.txt';
 
 axios.get(url, { responseType: 'stream' })
   .then((response) => {
-    // Pipe the response data to a writable stream (a file in this case)
+    const contentDisposition = response.headers['content-disposition'];
+
+    // Extract the filename from the content disposition header, if available
+    let filename = 'downloaded-file';
+    if (contentDisposition) {
+      const match = /filename="(.+)"/.exec(contentDisposition);
+      if (match) {
+        filename = match[1];
+      }
+    }
+
+    // Determine the file extension based on the content type, if available
+    const contentType = response.headers['content-type'];
+    const extFromContentType = contentType ? `.${contentType.split('/')[1]}` : '';
+
+    // Combine the filename and file extension
+    filename = filename.endsWith(extFromContentType) ? filename : filename + extFromContentType;
+
+    // Create a writable stream and pipe the response data to it
     const writer = fs.createWriteStream(filename);
     response.data.pipe(writer);
 
@@ -19,8 +35,8 @@ axios.get(url, { responseType: 'stream' })
     });
   })
   .then(() => {
-    console.log(`Content downloaded and saved as ${filename}`);
+    console.log(`File downloaded as ${filename}`);
   })
   .catch((error) => {
-    console.error('Error downloading content:', error);
+    console.error('Error downloading file:', error);
   });
