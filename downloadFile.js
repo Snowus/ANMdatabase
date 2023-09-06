5edc5faacf9d3327b5b08800e8120424bf2e518b
SNOWS <snows@ZRP-N012.localdomain>
2023-09-06 06:17:30 -0300

    one file download debug

--- downloadFile.js
+++ downloadFile.js
@@ -6,17 +6,18 @@ const path = require('path');
 const url = 'https://app.anm.gov.br/dadosabertos/SIGMINE/AREA_SERVIDAO.zip';
 
 axios.get(url, { responseType: 'stream' })
-  .then((response) => {
-    const contentDisposition = response.headers['content-disposition'];
-
+.then((response) => {
+  console.log(response)
+  console.log(url)
+  const contentDisposition = response.headers['content-disposition'];
+  
+  // Split the URL string by "/" to get an array of segments
+  const urlSegments = url.split('/');
+  // Access the "SIGMINE" and "AREA_SERVIDAO" parts
+  const folderToBeSaved = urlSegments[urlSegments.length - 2];
+  const fileNameToBeSaved = urlSegments[urlSegments.length - 1].split('.')[0];
     // Extract the filename from the content disposition header, if available
-    let filename = 'downloaded-file';
-    if (contentDisposition) {
-      const match = /filename="(.+)"/.exec(contentDisposition);
-      if (match) {
-        filename = match[1];
-      }
-    }
+  let filename = fileNameToBeSaved;
 
     // Determine the file extension based on the content type, if available
     const contentType = response.headers['content-type'];
@@ -24,6 +25,10 @@ axios.get(url, { responseType: 'stream' })
 
     // Combine the filename and file extension
     filename = filename.endsWith(extFromContentType) ? filename : filename + extFromContentType;
+
+    // Combine the desired directory path with the filename
+    const directoryPath = './' + folderToBeSaved; // Replace with your desired directory
+    const filePath = path.join(directoryPath, filename.endsWith(extFromContentType) ? filename : filename + extFromContentType);
 
     // Create a writable stream and pipe the response data to it
     const writer = fs.createWriteStream(filename);
@@ -35,7 +40,7 @@ axios.get(url, { responseType: 'stream' })
     });
   })
   .then(() => {
-    console.log(`File downloaded as ${filename}`);
+    console.log(`File downloaded as`);
   })
   .catch((error) => {
     console.error('Error downloading file:', error);
